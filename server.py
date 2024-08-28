592f9e5e71da296015194076c6cd80906dbd7f2e
Nihalrs <nihalravindra317@gmail.com>
2024-08-28 13:22:22 +0530

    The first commit

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import jsonify, request, Flask
+from database import *
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Welcome'
+
+if __name__=='__main__':
+    app.run()
